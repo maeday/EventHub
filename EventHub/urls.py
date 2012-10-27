fabb5f125f594b65573066380cd7e60b4d21fa66
Erik <ekchou@ymail.com>
2012-10-27 14:31:47 -0700

    Renamed 'my-event' to 'my-events'

--- EventHub/urls.py
+++ EventHub/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('django.views.generic.simple',
     # url(r'^EventHub/', include('EventHub.foo.urls')),
     url(r'^events/', include('events.urls')),
     url(r'^full-view$', 'direct_to_template', {'template': 'full-view.html'}),
-    url(r'^my-event$', 'direct_to_template', {'template': 'my-event.html'}),
+    url(r'^my-events$', 'direct_to_template', {'template': 'my-events.html'}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
