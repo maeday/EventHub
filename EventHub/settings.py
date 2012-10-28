5c8bc1cfb0faf73a0fbb6d7db3736290d9006a13
Erik <ekchou@ymail.com>
2012-10-27 17:50:10 -0700

    Added module 'django_localflavor_us'

--- EventHub/settings.py
+++ EventHub/settings.py
@@ -143,6 +143,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django_localflavor_us',
     'accounts',
     'events',
 )
